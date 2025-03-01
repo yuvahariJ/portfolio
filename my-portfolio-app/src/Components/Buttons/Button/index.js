276993b2d1b4b919145595e4265ed73660f89803
Yuvahari JR <yuvahari.j@gmail.com>
2025-03-01 20:50:06 +0530

    Added Icon button

--- my-portfolio-app/src/Components/Buttons/Button/index.js
+++ my-portfolio-app/src/Components/Buttons/Button/index.js
@@ -1,19 +1,34 @@
 import React from "react";
 import "./index.css";
+import { useTheme } from "../../../Context/ThemeProvider/Theme";
 
-
-const Button = ({...props } ) => {
-    const { label,onClick} = props;
-    return (
-        <button
-            {...onClick && ({
-                onClick: () => onClick()
-            })}
-            className="button-main"
-        >
-            {label}
-        </button>
-    )
-}
+const Button = ({ ...props }) => {
+  const { theme } = useTheme();
+  const { label, onClick, type, leadingIcon, style } = props;
+  const getButtonType = () => {
+    switch (type) {
+      case "outline":
+        if (theme) return "outline-black";
+        return "outline";
+      case "icon-button":
+        if (theme) return "icon-button  icon-button-dark";
+        return "icon-button  icon-button-light";
+      default:
+        return "button-main";
+    }
+  };
+  return (
+    <button
+      {...(onClick && {
+        onClick: () => onClick(),
+      })}
+      className={`button-default ${getButtonType()}`}
+      {...style}
+    >
+      {label}
+      {leadingIcon}
+    </button>
+  );
+};
 
 export default Button;
