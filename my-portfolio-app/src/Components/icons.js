e7e61737789f6017a457d234dc6a516c8f510793
Yuvahari JR <yuvahari.j@gmail.com>
2025-03-02 22:46:08 +0530

    Added Icons file

---
+++ my-portfolio-app/src/Components/icons.js
@@ -0,0 +1,14 @@
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { RiReactjsFill } from "react-icons/ri";
+import { RiNextjsFill } from "react-icons/ri";
+
+
+export  const IconsMap = {
+    Html: FaHtml5 ,
+    Css: FaCss3Alt,
+    JavaScript: IoLogoJavascript,
+    ReactJs: RiReactjsFill,
+    NextJs:RiNextjsFill,
+}
