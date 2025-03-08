cb31fb1bd4b116e6c5283fb5f1f87e3bfd979526
Yuvahari JR <yuvahari.j@gmail.com>
2025-03-08 20:24:05 +0530

    Task:11 - Added code to deploy my code

--- my-portfolio-app/src/App.js
+++ my-portfolio-app/src/App.js
@@ -1,27 +1,24 @@
-import "./App.css";
-import { useTheme } from "./Context/ThemeProvider/Theme";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Pages/Header";
-import LoadingPage from "./Components/Loading";
-import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Resume from "./Pages/Resume";
 import Contact from "./Pages/Contact";
+import { useTheme } from "./Context/ThemeProvider/Theme";
 
 function App() {
   const { theme } = useTheme();
   return (
-    <>
+    <Router basename="/portfolio"> {/* 👈 Add basename here */}
       <div id="main-div" className={theme ? "dark-theme" : "light-theme"}>
         OM
         <Header />
-        <Routes basename="/portfolio">
+        <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        {/* <LoadingPage></LoadingPage> */}
       </div>
-    </>
+    </Router>
   );
 }
 
